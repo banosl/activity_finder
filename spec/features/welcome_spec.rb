c85a7c73f4cc53aa1d57348545f678971520d710
Leo Banos Garcia <bsleo625@gmail.com>
2023-02-05 15:20:05 -0700

    adds view page for /activity with the needed controller and routing

--- spec/features/welcome_spec.rb
+++ spec/features/welcome_spec.rb
@@ -1,11 +1,24 @@
 require 'rails_helper'
 
 RSpec.describe "Welcome Page" do 
-    it 'has a form to search for activities' do 
-        visit root_path
-        
-        expect(page).to have_content("Activity Finder")
-        expect(page).to have_field(:num_participants)
-        expect(page).to have_button("Find Activities")
-    end 
+    describe "visiting '/'" do
+        it 'has a form to search for activities' do 
+            visit root_path
+            
+            expect(page).to have_content("Activity Finder")
+            expect(page).to have_field(:num_participants)
+            expect(page).to have_button("Find Activities")
+        end
+
+        it 'takes user to /activity after clicking Find Something To do' do
+            visit root_path
+
+            fill_in :num_participants, with: 3
+            click_on "Find Activities"
+
+            expect(current_path).to eq('/activity')
+        end
+
+        it 'user sees a description of suggested activity, the number of participants for the activity, and a visual of how pricey that activity would be'
+    end
 end 
